fa93e5f8e0d4560ab54ae09f57a96c8467eeb222
andriani <a.christanty@gmail.com>
2022-04-26 19:54:45 +0800

    deleted numpy

--- internet_app.py
+++ internet_app.py
@@ -4,7 +4,6 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-import numpy as np
 import pandas as pd
 import json
 from flask import Flask, jsonify,render_template
